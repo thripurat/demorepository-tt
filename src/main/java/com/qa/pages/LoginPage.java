0959bf024dc1ea0df01cff8eb656937f5e2c31be
thripurat <152591291+thripurat@users.noreply.github.com>
2023-12-02 11:08:43 +0530

    Update LoginPage.java

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -6,10 +6,18 @@ public void loginpage()
 {
 	System.out.println("loginpage");
 	
+	
 }
 public void login2page()
 {
 	System.out.println("login2updatedpage");
 }
+
+public void gitloginupdate()
+	{
+	System.out.println("gitloginupdate");
+}	
 }
+
+
 
